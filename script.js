74e84cfc31c84913acca38101e13c8a947c392fa
n1njayden <94377575+n1njayden@users.noreply.github.com>
2022-01-26 13:51:31 +0100

    index fixed

--- script.js
+++ script.js
@@ -25,27 +25,3 @@ function sendReviews(){
 
 document.getElementById("knop").addEventListener('click', sendReviews);
 
-
-
-var slideIndex = 0;
-
-function showSlides() {
-  var i;
-  var dots = document.getElementsByClassName("dot")
-  var slides = document.getElementsByClassName("Slides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 3000); // Change image every 2 seconds
-
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
-
-showSlides();
